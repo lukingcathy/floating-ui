cbfb2edc6bbf4a74cd06ecdcbf595bd92eb75273
lukingcathy <cathyluking@foxmail.com>
2024-09-28 01:15:33 +0800

    chore: 添加注释，方便理解

--- crates/utils/lib.rs
+++ crates/utils/lib.rs
@@ -10,21 +10,30 @@ use serde::{Deserialize, Serialize};
 #[cfg(feature = "dom")]
 pub mod dom;
 
+/// 对齐，每一个方位有三个对齐位置，默认中间
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Alignment {
+    /// 距离原点近的位置，对于左右的方位而言，这个就上边的点；对于上下的方位而言，这个就是左边的点
     Start,
+    /// 距离原点远的位置，对于左右的方位而言，这个就下边的点；对于上下的方位而言，这个就是右边的点
     End,
 }
 
+/// 方位
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Side {
+    /// 上
     Top,
+    /// 右
     Right,
+    /// 下
     Bottom,
+    /// 左
     Left,
 }
 
 impl Side {
+    /// 翻转方位
     pub fn opposite(&self) -> Side {
         use Side::{Bottom, Left, Right, Top};
         match self {
@@ -35,6 +44,7 @@ impl Side {
         }
     }
 
+    /// 获取每个方位所在的坐标轴
     pub fn axis(&self) -> Axis {
         use Side::{Bottom, Left, Right, Top};
         match self {
@@ -44,35 +54,58 @@ impl Side {
     }
 }
 
+/// 和方位结合，允许浮动元素对齐的位置点
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum AlignedPlacement {
+    /// 上左
     TopStart,
+    /// 上右
     TopEnd,
+    /// 右上
     RightStart,
+    /// 右下
     RightEnd,
+    /// 下左
     BottomStart,
+    /// 下右
     BottomEnd,
+    /// 左上
     LeftStart,
+    /// 左下
     LeftEnd,
 }
 
+/// 浮动元素放置的位置
 #[derive(Copy, Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub enum Placement {
+    /// 上，默认中间
     Top,
+    /// 上左
     TopStart,
+    /// 上右
     TopEnd,
+    /// 右，默认中间
     Right,
+    /// 右上
     RightStart,
+    /// 右下
     RightEnd,
+    /// 下，默认中间
     Bottom,
+    /// 下左
     BottomStart,
+    /// 下右
     BottomEnd,
+    /// 左，默认中间
     Left,
+    /// 左上
     LeftStart,
+    /// 左下
     LeftEnd,
 }
 
 impl Placement {
+    /// 通过浮动元素放置的位置获取对齐
     pub fn alignment(&self) -> Option<Alignment> {
         use Placement::{
             Bottom, BottomEnd, BottomStart, Left, LeftEnd, LeftStart, Right, RightEnd, RightStart,
@@ -85,6 +118,7 @@ impl Placement {
         }
     }
 
+    /// 通过浮动元素放置的位置获取方位
     pub fn side(&self) -> Side {
         use Placement::{
             Bottom, BottomEnd, BottomStart, Left, LeftEnd, LeftStart, Right, RightEnd, RightStart,
@@ -98,6 +132,7 @@ impl Placement {
         }
     }
 
+    /// 根据浮动元素放置的位置获取翻转后的位置
     pub fn opposite(&self) -> Placement {
         use Placement::{
             Bottom, BottomEnd, BottomStart, Left, LeftEnd, LeftStart, Right, RightEnd, RightStart,
@@ -119,6 +154,7 @@ impl Placement {
         }
     }
 
+    /// 根据浮动元素放置的位置获取翻转的对齐点
     pub fn opposite_alignment(&self) -> Placement {
         use Placement::{
             Bottom, BottomEnd, BottomStart, Left, LeftEnd, LeftStart, Right, RightEnd, RightStart,
@@ -143,36 +179,45 @@ impl Placement {
 
 impl From<(Side, Option<Alignment>)> for Placement {
     fn from(value: (Side, Option<Alignment>)) -> Self {
+        use Alignment::{End, Start};
+        use Side::{Bottom, Left, Right, Top};
         match value {
-            (Side::Top, None) => Placement::Top,
-            (Side::Top, Some(Alignment::Start)) => Placement::TopStart,
-            (Side::Top, Some(Alignment::End)) => Placement::TopEnd,
-            (Side::Right, None) => Placement::Right,
-            (Side::Right, Some(Alignment::Start)) => Placement::RightStart,
-            (Side::Right, Some(Alignment::End)) => Placement::RightEnd,
-            (Side::Bottom, None) => Placement::Bottom,
-            (Side::Bottom, Some(Alignment::Start)) => Placement::BottomStart,
-            (Side::Bottom, Some(Alignment::End)) => Placement::BottomEnd,
-            (Side::Left, None) => Placement::Left,
-            (Side::Left, Some(Alignment::Start)) => Placement::LeftStart,
-            (Side::Left, Some(Alignment::End)) => Placement::LeftEnd,
+            (Top, None) => Placement::Top,
+            (Top, Some(Start)) => Placement::TopStart,
+            (Top, Some(End)) => Placement::TopEnd,
+            (Right, None) => Placement::Right,
+            (Right, Some(Start)) => Placement::RightStart,
+            (Right, Some(End)) => Placement::RightEnd,
+            (Bottom, None) => Placement::Bottom,
+            (Bottom, Some(Start)) => Placement::BottomStart,
+            (Bottom, Some(End)) => Placement::BottomEnd,
+            (Left, None) => Placement::Left,
+            (Left, Some(Start)) => Placement::LeftStart,
+            (Left, Some(End)) => Placement::LeftEnd,
         }
     }
 }
 
+/// 放置的策略
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Strategy {
+    /// 绝对值
     Absolute,
+    /// 固定值
     Fixed,
 }
 
+/// 坐标轴
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Axis {
+    /// X 轴
     X,
+    /// Y 轴
     Y,
 }
 
 impl Axis {
+    /// 通过当前轴获取翻转的轴
     pub fn opposite(&self) -> Axis {
         use Axis::{X, Y};
         match self {
@@ -181,6 +226,7 @@ impl Axis {
         }
     }
 
+    /// 获取长度标识， X 轴应该获取元素的宽度，Y 轴获取元素的高度
     pub fn length(&self) -> Length {
         use Axis::{X, Y};
         match self {
@@ -190,6 +236,7 @@ impl Axis {
     }
 }
 
+/// 元素坐标
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Coords {
     pub x: f64,
@@ -201,6 +248,7 @@ impl Coords {
         Self { x: value, y: value }
     }
 
+    /// 根据坐标轴获取对应坐标值
     pub fn axis(&self, axis: Axis) -> f64 {
         use Axis::{X, Y};
         match axis {
@@ -209,15 +257,18 @@ impl Coords {
         }
     }
 
+    /// 在指定的坐标轴上进行偏移
     pub fn update_axis<F>(&mut self, axis: Axis, update: F)
     where
         F: Fn(f64) -> f64,
     {
+        use Axis::{X, Y};
+
         match axis {
-            Axis::X => {
+            X => {
                 self.x = update(self.x);
             }
-            Axis::Y => {
+            Y => {
                 self.y = update(self.y);
             }
         }
